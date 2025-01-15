0401693ee4b21fed67591160ad822e488ddc2cca
kartik <kartikvyas02@gmail.com>
2025-01-15 13:39:20 +0530

    second commit

--- backend/controller/adminController.js
+++ backend/controller/adminController.js
@@ -1,20 +1,45 @@
-const Admin = require('../model/adminModel');
-const bcrypt = require('bcrypt');
+const Admin = require("../model/adminModel");
+const bcrypt = require("bcrypt");
 
 module.exports.login = async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        const admin = await Admin.findOne({ username });
-        if (!admin) {
-            return res.status(404).json({ error: 'Admin not found' });
-        }
-        const passwordMatch = await bcrypt.compare(password, admin.password);
-        if (!passwordMatch) {
-            return res.status(401).json({ error: 'Invalid credentials' });
-        }
-        res.status(200).json({ message: 'Login successful' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal server error' });
+  try {
+    const { username, password } = req.body;
+    const admin = await Admin.findOne({ username });
+    if (!admin) {
+      return res.status(404).json({ error: "Admin not found" });
     }
+    const passwordMatch = await bcrypt.compare(password, admin.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+    res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+module.exports.register = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const existingAdmin = await Admin.findOne({ username });
+    if (existingAdmin) {
+      return res.status(400).json({ error: "Username already taken" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newAdmin = new Admin({
+      username,
+      password: hashedPassword,
+    });
+
+    await newAdmin.save();
+
+    res.status(201).json({ message: "Admin registered successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { login} = require('../controller/adminController'); 
+const { login, register } = require("../controller/adminController");
 
-router.post('/login',login);
+router.post("/login", login);
+router.post("/register", register);
 
-
-module.exports = router
+module.exports = router;
